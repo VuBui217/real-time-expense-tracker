0cb162c4579c08ff61608772f90b04daf21925c1
Phuc <phucgianguyen0199@gmail.com>
2024-09-10 13:09:34 -0400

    Initialize the postgres

--- backend/config.py
+++ backend/config.py
@@ -6,11 +6,16 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///site.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:610199@localhost:5433/expense_tracker_user",
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class TestConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"  # Use in-memory database for testing
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql://postgres:610199@localhost:5433/expense_tracker_user"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
